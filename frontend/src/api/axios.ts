5e8d3898727d97ceaeb66040ce7f47d56b260ee6
Shyam-423 <shyam2410533@ssn.edu.in>
2026-07-10 14:06:18 +0530

    Frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://goalwise-ai-f8lm.onrender.com",
   withCredentials: true,
 });
 
