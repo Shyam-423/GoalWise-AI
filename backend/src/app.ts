47f5093e4fe69aeb7a7caf5321d39ec328f30b6b
Shyam-423 <shyam2410533@ssn.edu.in>
2026-07-10 14:18:18 +0530

    app.use

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,10 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL!,
+    ],
     credentials: true,
   })
 );
